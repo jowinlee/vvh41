30e3387ca94ef2a36108ef9c695fad9d409e870e
Elisha <lahzambo.alwinrosal@gmail.com>
2019-08-05 10:29:32 +0800

    register page trans class

--- registrationPage/registration.php
+++ registrationPage/registration.php
@@ -8,7 +8,7 @@
 
 <?php else: ?>
     <div class="card">
-        <h5 class="card-header special-color white-text text-center py-4">
+        <h5 class="card-header special-color white-text text-center py-4" lang="en">
             <strong>Create Account</strong>
         </h5>
         <div class="card-body px-lg-5 pt-0">
@@ -17,23 +17,23 @@
                     <div class="col">
                         <div class="md-form">
                             <input type="text" name="firstname" id="firstname" class="form-control" required="required">
-                            <label for="firstname">First name</label>
+                            <label for="firstname" lang="en">First name</label>
                         </div>
                     </div>
                     <div class="col">
                         <div class="md-form">
                             <input type="text" name="lastname" id="lastname" class="form-control" required="required">
-                            <label for="lastname">Last name</label>
+                            <label for="lastname" lang="en">Last name</label>
                         </div>
                     </div>
                 </div>
                 <div class="md-form mt-0">
                     <input type="email" name="email" id="email" class="form-control" required="required">
-                    <label for="email">E-mail</label>
+                    <label for="email" lang="en">E-mail</label>
                 </div>
-                <button class="btn btn-success btn-rounded btn-block my-4 z-depth-0" type="submit" id="regBtn">Proceed to Templates</button>
+                <button class="btn btn-success btn-rounded btn-block my-4 z-depth-0" type="submit" id="regBtn" lang="en">Proceed to Templates</button>
             </form>
-            <p>Already has an account? <a href="">Login</a></p>
+            <p lang="en">Already has an account?<a href="" lang="en">Login</a></p>
         </div>
     </div>
 <?php endif; ?>
