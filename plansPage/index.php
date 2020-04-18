5bc8a4afbea17bf141eb41af1295ce7f5c2eed88
desu <denzdesu@gmail.com>
2020-04-18 16:44:50 +0800

    publish flow lightbox content

---
+++ plansPage/index.php
@@ -0,0 +1,112 @@
+<?php include('../general/head.php');?>
+<div class="container my-5">
+    <div class="card-group">
+        <div class="card mb-4">
+            <div class="view overlay">
+                <img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Others/images/49.jpg" alt="Card image cap">
+                <a href="#!">
+                    <div class="mask rgba-white-slight"></div>
+                </a>
+            </div>
+            <div class="card-body warning-color-dark text-white">
+                <h4 class="card-title">Personnel</h4>
+                <p class="card-text text-white">11.00€ <small>/annuel</small></p>
+                <button type="button" class="btn btn-primary btn-md">Acheter</button>
+            </div>
+        </div>
+        <div class="card mb-4">
+            <div class="view overlay">
+                <img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Others/images/48.jpg" alt="Card image cap">
+                <a href="#!">
+                    <div class="mask rgba-white-slight"></div>
+                </a>
+            </div>
+            <div class="card-body warning-color-dark text-white">
+                <h4 class="card-title">Entreprice</h4>
+                <p class="card-text text-white">17.00€ <small>/annuel</small></p>
+                <button type="button" class="btn btn-primary btn-md">Acheter</button>
+            </div>
+        </div>
+        <div class="card mb-4">
+            <div class="view overlay">
+                <img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Others/images/77.jpg" alt="Card image cap">
+                <a href="#!">
+                    <div class="mask rgba-white-slight"></div>
+                </a>
+            </div>
+            <div class="card-body warning-color-dark text-white">
+                <h4 class="card-title">Commerce Basic</h4>
+                <p class="card-text text-white">24.00€ <small>/annuel</small></p>
+                <button type="button" class="btn btn-primary btn-md">Acheter</button>
+            </div>  
+        </div>
+        <div class="card mb-4">
+            <div class="view overlay">
+                <img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Others/images/50.jpg" alt="Card image cap">
+                <a href="#!">
+                    <div class="mask rgba-white-slight"></div>
+                </a>
+            </div>
+            <div class="card-body warning-color-dark text-white">
+                <h4 class="card-title">Commerce Avancé</h4>
+                <p class="card-text text-white">36.00€ <small>/annuel</small></p>
+                <button type="button" class="btn btn-primary btn-md">Acheter</button>
+            </div>  
+        </div>  
+    </div>
+    <div class="card-group">
+        <div class="card mb-4">
+            <div class="view overlay">
+                <img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Others/images/49.jpg" alt="Card image cap">
+                <a href="#!">
+                    <div class="mask rgba-white-slight"></div>
+                </a>
+            </div>
+            <div class="card-body primary-color-dark text-white">
+                <h4 class="card-title">Personnel</h4>
+                <p class="card-text text-white">15.00€ <small>/mensuel</small></p>
+                <button type="button" class="btn btn-primary btn-md">Acheter</button>
+            </div>
+        </div>
+        <div class="card mb-4">
+            <div class="view overlay">
+                <img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Others/images/48.jpg" alt="Card image cap">
+                <a href="#!">
+                    <div class="mask rgba-white-slight"></div>
+                </a>
+            </div>
+            <div class="card-body primary-color-dark text-white">
+                <h4 class="card-title">Entreprice</h4>
+                <p class="card-text text-white">21.00€ <small>/mensuel</small></p>
+                <button type="button" class="btn btn-primary btn-md">Acheter</button>
+            </div>
+        </div>
+        <div class="card mb-4">
+            <div class="view overlay">
+                <img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Others/images/77.jpg" alt="Card image cap">
+                <a href="#!">
+                    <div class="mask rgba-white-slight"></div>
+                </a>
+            </div>
+            <div class="card-body primary-color-dark text-white">
+                <h4 class="card-title">Commerce Basic</h4>
+                <p class="card-text text-white">28.00€ <small>/mensuel</small></p>
+                <button type="button" class="btn btn-primary btn-md">Acheter</button>
+            </div>  
+        </div>
+        <div class="card mb-4">
+            <div class="view overlay">
+                <img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Others/images/50.jpg" alt="Card image cap">
+                <a href="#!">
+                    <div class="mask rgba-white-slight"></div>
+                </a>
+            </div>
+            <div class="card-body primary-color-dark text-white">
+                <h4 class="card-title">Commerce Avancé</h4>
+                <p class="card-text text-white">40.00€ <small>/mensuel</small></p>
+                <button type="button" class="btn btn-primary btn-md">Acheter</button>
+            </div>  
+        </div>  
+    </div>
+</div>
+<?php include('../general/foot.php');?>
