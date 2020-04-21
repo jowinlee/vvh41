e20fc64fd959d4cc0e0562c7f6fd748f51318408
desu <denzdesu@gmail.com>
2020-04-21 21:33:50 +0800

    paypal link update

--- plansPage/index.php
+++ plansPage/index.php
@@ -4,53 +4,53 @@
         <div class="card mb-4">
             <div class="view overlay">
                 <img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Others/images/49.jpg" alt="Card image cap">
-                <a href="#!">
+                <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=64NJWMFRXLDCS">
                     <div class="mask rgba-white-slight"></div>
                 </a>
             </div>
             <div class="card-body warning-color-dark text-white">
                 <h4 class="card-title">Personnel</h4>
                 <p class="card-text text-white">11.00€ <small>/annuel</small></p>
-                <button type="button" class="btn btn-primary btn-md">Acheter</button>
+                <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=64NJWMFRXLDCS" class="btn btn-primary btn-md">Acheter</a>
             </div>
         </div>
         <div class="card mb-4">
             <div class="view overlay">
                 <img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Others/images/48.jpg" alt="Card image cap">
-                <a href="#!">
+                <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=6PVJR4JYHUSD4">
                     <div class="mask rgba-white-slight"></div>
                 </a>
             </div>
             <div class="card-body warning-color-dark text-white">
                 <h4 class="card-title">Entreprice</h4>
                 <p class="card-text text-white">17.00€ <small>/annuel</small></p>
-                <button type="button" class="btn btn-primary btn-md">Acheter</button>
+                <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=6PVJR4JYHUSD4" class="btn btn-primary btn-md">Acheter</a>
             </div>
         </div>
         <div class="card mb-4">
             <div class="view overlay">
                 <img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Others/images/77.jpg" alt="Card image cap">
-                <a href="#!">
+                <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=7EZVXHSW2UEY8">
                     <div class="mask rgba-white-slight"></div>
                 </a>
             </div>
             <div class="card-body warning-color-dark text-white">
                 <h4 class="card-title">Commerce Basic</h4>
                 <p class="card-text text-white">24.00€ <small>/annuel</small></p>
-                <button type="button" class="btn btn-primary btn-md">Acheter</button>
+                <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=7EZVXHSW2UEY8" class="btn btn-primary btn-md">Acheter</a>
             </div>  
         </div>
         <div class="card mb-4">
             <div class="view overlay">
                 <img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Others/images/50.jpg" alt="Card image cap">
-                <a href="#!">
+                <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=NKQJS5W8PZMXW">
                     <div class="mask rgba-white-slight"></div>
                 </a>
             </div>
             <div class="card-body warning-color-dark text-white">
                 <h4 class="card-title">Commerce Avancé</h4>
                 <p class="card-text text-white">36.00€ <small>/annuel</small></p>
-                <button type="button" class="btn btn-primary btn-md">Acheter</button>
+                <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=NKQJS5W8PZMXW" class="btn btn-primary btn-md">Acheter</a>
             </div>  
         </div>  
     </div>
@@ -58,53 +58,53 @@
         <div class="card mb-4">
             <div class="view overlay">
                 <img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Others/images/49.jpg" alt="Card image cap">
-                <a href="#!">
+                <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=T92GPQDPSCBJW">
                     <div class="mask rgba-white-slight"></div>
                 </a>
             </div>
             <div class="card-body primary-color-dark text-white">
                 <h4 class="card-title">Personnel</h4>
                 <p class="card-text text-white">15.00€ <small>/mensuel</small></p>
-                <button type="button" class="btn btn-primary btn-md">Acheter</button>
+                <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=T92GPQDPSCBJW" class="btn btn-primary btn-md">Acheter</a>
             </div>
         </div>
         <div class="card mb-4">
             <div class="view overlay">
                 <img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Others/images/48.jpg" alt="Card image cap">
-                <a href="#!">
+                <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=5H869R7K3BL62">
                     <div class="mask rgba-white-slight"></div>
                 </a>
             </div>
             <div class="card-body primary-color-dark text-white">
                 <h4 class="card-title">Entreprice</h4>
                 <p class="card-text text-white">21.00€ <small>/mensuel</small></p>
-                <button type="button" class="btn btn-primary btn-md">Acheter</button>
+                <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=5H869R7K3BL62" class="btn btn-primary btn-md">Acheter</a>
             </div>
         </div>
         <div class="card mb-4">
             <div class="view overlay">
                 <img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Others/images/77.jpg" alt="Card image cap">
-                <a href="#!">
+                <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=A4LB7T7HW9XJ8">
                     <div class="mask rgba-white-slight"></div>
                 </a>
             </div>
             <div class="card-body primary-color-dark text-white">
                 <h4 class="card-title">Commerce Basic</h4>
                 <p class="card-text text-white">28.00€ <small>/mensuel</small></p>
-                <button type="button" class="btn btn-primary btn-md">Acheter</button>
+                <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=A4LB7T7HW9XJ8" class="btn btn-primary btn-md">Acheter</a>
             </div>  
         </div>
         <div class="card mb-4">
             <div class="view overlay">
                 <img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Others/images/50.jpg" alt="Card image cap">
-                <a href="#!">
+                <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=JK2CTUZXZVJWA">
                     <div class="mask rgba-white-slight"></div>
                 </a>
             </div>
             <div class="card-body primary-color-dark text-white">
                 <h4 class="card-title">Commerce Avancé</h4>
                 <p class="card-text text-white">40.00€ <small>/mensuel</small></p>
-                <button type="button" class="btn btn-primary btn-md">Acheter</button>
+                <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=JK2CTUZXZVJWA" class="btn btn-primary btn-md">Acheter</a>
             </div>  
         </div>  
     </div>
