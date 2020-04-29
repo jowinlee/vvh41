dbd301b6adc3ed1a7c8b64a8e532b163ceb4c53a
desu <denzdesu@gmail.com>
2020-04-30 01:19:09 +0800

    paypal transaction update

--- checkoutsuccess/index.php
+++ checkoutsuccess/index.php
@@ -6,8 +6,8 @@
     $publishedsite = publishSite($sitename);
 ?>
 <div class="container mt-5">
-    <h2 class="text-success">Thank you for your purchase <? echo $clientname; ?>!</h2>
+    <h2 class="text-success">Thank you for your purchase <?php echo $clientname ?>!</h2>
     <h4>Your site has been successfully Published.</h4>
-    <p><a class="text-info" href="<? echo $publishedsite; ?>">Go to my Website</a></p>
+    <p><a class="text-info" href="<?php echo $publishedsite ?>">Go to my Website</a></p>
 </div>
 <?php include('../general/foot.php');?>
