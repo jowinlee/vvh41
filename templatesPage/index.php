20e7312f0084b79b60ffeef6b704211f817bdbca
Elisha <lahzambo.alwinrosal@gmail.com>
2020-05-10 22:15:49 +0800

    remove the toggle button

--- templatesPage/index.php
+++ templatesPage/index.php
@@ -34,7 +34,7 @@
     <!-- /#sidebar-wrapper -->
             <div id="page-content-wrapper" class="">
                 <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom vvh-blue">
-                    <button class="btn btn-primary" id="menu-toggle">Toggle Menu</button>
+                    <!--<button class="btn btn-primary" id="menu-toggle">Toggle Menu</button>-->
 
                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                       <span class="navbar-toggler-icon"></span>
