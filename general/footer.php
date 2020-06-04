142cfd5ab4c7f1a74ea0516724831d355de42a99
desu <denzdesu@gmail.com>
2020-06-04 23:47:25 +0800

    footer design update

--- general/footer.php
+++ general/footer.php
@@ -1,95 +1,74 @@
 	</main>
-	<footer class="page-footer font-small elegant-color vvh-black" style="margin-top: 50px; color: #fff;">
-            <div class="container py-5">
-            	<div class="row">
-            		<div class="col">
-            			<a href="https://www.concepteur-site-web.fr">
-                                    <img class="img-fluid mx-auto d-block vvh-logo-footer" src="../img/vvh4.png">
-                              </a>
-            			<p class="text-center">
-                               <small>
-                                    Notre mission?
-                                    Nous aidons les particuliers, entreprises et  entrepreneurs à se développer grâce à un site web pertinent, pour leur permettre d’attirer jusqu’à 5 fois plus de clients grâce à notre expertise.
-                              </small>
-                               </p>
-            		</div>
-            		<div class="col">
-            			<h3 class="text-uppercase mb-4">Contactez-nous</h3>
-                                   <p><a href="mailto:contact@virtualvisiblehands.com" runtime_url="mailto:contact@virtualvisiblehands.com" style="color: rgb(240, 90, 40);">
-                                          <font style="color: rgb(240, 90, 40);">contact@virtualvisiblehands.com</font>
-                                    </a></p>
-                                    <p>
-                                      <a href="https://irp-cdn.multiscreensite.com/1a7979e6/files/uploaded/CGV_CGU%20VVH%208_03_2019.docx.pdf" file="true" runtime_url="https://irp-cdn.multiscreensite.com/1a7979e6/files/uploaded/CGV_CGU%20VVH%208_03_2019.docx.pdf" target="_blank" no_space_b="true" no_space_e="true" style="background-color: rgba(0, 0, 0, 0);">
-                                     <span no_space_b="true" no_space_e="true">
-                                      <font style="color: rgb(240, 90, 40);" no_space_b="true" no_space_e="true">
-                                       Condition G&eacute;n&eacute;rale d'utilisation
-                                      </font>
-                                     </span>
-                                    </a></p>
-
-                                    Vous avez déjà un compte ? Bienvenue chez VOUS !
-
-                                    <p>
-                                    <a href="tel:0767278474" runtime_url="tel:0767278474" style="">
-                                          <font style="color: rgb(240, 90, 40);">0767278474</font>
-                                    </a>
-                              </p>
-                              <p>
-                                   <!--  <a href="mailto:contact@virtualvisiblehands.com" runtime_url="mailto:contact@virtualvisiblehands.com" style="color: rgb(240, 90, 40);">
-                                          <font style="color: rgb(240, 90, 40);">contact@virtualvisiblehands.com</font>
-                                    </a> -->
-                              </p>
-                              <p>FRANCE</p>
-                              <p>
-                                    <span class="navbar-text white-text">
-                                          Already have an account? 
-                                          <a class="text-danger" href="https://createur.virtualvisiblehands.com">LOGIN HERE</a>
-                                    </span>
-                              </p>
-                              </p>
-            			<ul class="list-unstyled">
-            				<li><p><a href="https://www.concepteur-site-web.fr/">Accuel</a></p></li>
-            				<li><p><a href="https://www.concepteur-site-web.fr/notre-offre">Notre Offre</a></p></li>
-            				<li><p><a href="https://www.concepteur-site-web.fr/#Contact">Contact</a></p></li>
-            				<li><p><a href="https://www.concepteur-site-web.fr/solutions-innovantes-web-blog">Blog</a></p></li>
-            				<li><p><a href="http://environnement-travail.concepteur-site-web.fr/vvh41/templatesPage/">Templates</a></p></li>
-                                    <li><p><a href="http://environnement-travail.concepteur-site-web.fr/vvh41/forfaits-VirtualVisibleHands-conception-autonome-accompagné/">ForFaits</a></p></li>
-            			</ul>
-            		</div>
-            		<!-- <div class="col">
-            			<h3 class="text-uppercase mb-4">Contact us</h3>
-            			
-            		</div> -->
-            		<div class="col">
-            			<h3 class="text-uppercase mb-4">Suivez nous</h3>
-            			<ul class="list-inline">
-            				<li class="list-inline-item">
-                                          <a href="https://www.facebook.com/CreateurSiteReactifAdaptatifPro/">
-                                                <i class="fab fa-facebook-f"></i>
-                                          </a>
-                                    </li>
-            				<li class="list-inline-item">
-                                          <a href="https://www.linkedin.com/in/virtual-visible-hands-v-v-h-90805a176">
-                                                <i class="fab fa-linkedin-in"></i>
-                                          </a>
-                                    </li>
-            			</ul>
-            			<ul class="list-inline">
-            				<li class="list-inline-item">
-                                          <a href="https://twitter.com/VVH_Pro">
-                                                <i class="fab fa-twitter"></i>
-                                          </a>
-                                    </li>
-            				<li class="list-inline-item">
-                                          <a href="https://www.youtube.com/user/UCCQNrlL_g5JiJLSu3k_cBmA">
-                                                <i class="fab fa-youtube"></i>
-                                          </a>
-                                    </li>
-            			</ul>      			
-            		</div>
+	<footer class="page-footer font-small vvh-trans-blue" style="margin-top: 50px; color: #fff;background-color: #0c1c3a;">
+        <div class="container py-5">
+           	<div class="row">
+           		<div class="col">
+           			<a href="https://www.concepteur-site-web.fr">
+                        <img class="img-fluid mx-auto d-block vvh-logo-footer" src="../img/vvh4.png">
+                    </a>
+                    <h5 class="text-center"><strong>Notre mission?</strong></h5>
+           			<p class="text-center small">
+                        Nous aidons les particuliers, entreprises et
+                        entrepreneurs à se développer grâce à un site web 
+                        pertinent, pour leur permettre d’attirer jusqu’à 5 fois
+                        plus de clients grâce à notre expertise.
+                    </p>
+           		</div>
+           		<div class="col">
+       			    <h5 class="text-uppercase mb-4"><strong>Contactez-nous</strong></h5>
+                    <p>
+                        <a href="mailto:contact@virtualvisiblehands.com" runtime_url="mailto:contact@virtualvisiblehands.com" style="color: rgb(240, 90, 40);">
+                            <font style="color: rgb(240, 90, 40);">contact@virtualvisiblehands.com</font>
+                        </a>
+                    </p>
+                    <p>
+                        <a href="https://irp-cdn.multiscreensite.com/1a7979e6/files/uploaded/CGV_CGU%20VVH%208_03_2019.docx.pdf" file="true" runtime_url="https://irp-cdn.multiscreensite.com/1a7979e6/files/uploaded/CGV_CGU%20VVH%208_03_2019.docx.pdf" target="_blank" no_space_b="true" no_space_e="true" style="background-color: rgba(0, 0, 0, 0);">
+                            <span no_space_b="true" no_space_e="true">
+                                <font style="color: rgb(240, 90, 40);" no_space_b="true" no_space_e="true">
+                                    Condition G&eacute;n&eacute;rale d'utilisation
+                                </font>
+                            </span>
+                        </a>
+                    </p>
+                    <p>
+                        Vous avez déjà un compte ?<br> 
+                        Bienvenue chez <a href="https://createur.virtualvisiblehands.com/home" runtime_url="https://createur.virtualvisiblehands.com/home" target="_blank" style="color: rgb(240, 90, 40);">VOUS</a> !
+                    </p>
+        		</div>
+            	<div class="col">
+            		<h5 class="text-uppercase mb-4"><strong>Suivez nous</strong></h5>
+            		<ul class="list-inline">
+            			<li class="list-inline-item">
+                            <a href="https://www.facebook.com/CreateurSiteReactifAdaptatifPro/">
+                                <i class="fab fa-facebook-f"></i>
+                            </a>
+                        </li>
+            			<li class="list-inline-item">
+                            <a href="https://twitter.com/VVH_Pro">
+                                <i class="fab fa-twitter"></i>
+                            </a>
+                        </li>
+            			<li class="list-inline-item">
+                            <a href="https://www.youtube.com/user/UCCQNrlL_g5JiJLSu3k_cBmA">
+                                <i class="fab fa-youtube"></i>
+                            </a>
+                        </li>
+                        <li class="list-inline-item">
+                            <a href="https://www.linkedin.com/in/virtual-visible-hands-v-v-h-90805a176">
+                                <i class="fab fa-linkedin-in"></i>
+                            </a>
+                        </li>
+                        <li class="list-inline-item">
+                            <a href="https://www.contact@virtualvisiblehands.com">
+                                <i class="fa fa-envelope"></i>
+                            </a>
+                        </li>
+            		</ul>
+                    <p class="small">En souscrivant à la newsletter VVH, j'accepte de recevoir des mails sur ses actualités, vous pouvez à tout moment vous désinscrire.</p>     			
             	</div>
             </div>
-      </footer>    
+        </div>
+    </footer>    
 	<script type="text/javascript" src="../js/jquery-3.4.1.min.js"></script>
 	<script type="text/javascript" src="../js/popper.min.js"></script>
 	<script type="text/javascript" src="../js/bootstrap.min.js"></script>
