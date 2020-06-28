e0ee7001844aee2ea6ef33608f2ea5f33989755a
sarah <sarah.guiritan@virtualvisiblehands.com>
2020-06-29 00:14:39 +0800

    updated the api username and password

--- api/api.php
+++ api/api.php
@@ -1,6 +1,6 @@
 <?php 
 	//Set API user and password
-	define("API_USER","2cc08a6de9");
-	define("API_PASS","rgzz9cy354Pb");
+	define("API_USER","21a8a02175");//21a8a02175
+	define("API_PASS","YbUN6Q74wolb");//YbUN6Q74wolb
 	//define("API_PASS","Em3mW5Xv9pS1");
 ?>
