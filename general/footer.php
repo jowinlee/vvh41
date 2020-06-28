74920fd4ac01dccfe59778e079c2aa2a348ac11b
sarah <sarah.guiritan@virtualvisiblehands.com>
2020-06-29 00:22:18 +0800

    remove language translator

--- general/footer.php
+++ general/footer.php
@@ -73,11 +73,11 @@
 	<script type="text/javascript" src="../js/popper.min.js"></script>
 	<script type="text/javascript" src="../js/bootstrap.min.js"></script>
 	<script type="text/javascript" src="../js/mdb.min.js"></script>
-      <script type="text/javascript">
+     <!--  <script type="text/javascript">
             function googleTranslateElementInit() {
               new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
             }
       </script>
-      <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+      <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script> -->
   </body>
 </html>
