3923e875257918377fe23d160023f1ce92280767
desu <denzdesu@gmail.com>
2020-07-05 16:53:26 +0800

    api new pass

--- api/api.php
+++ api/api.php
@@ -1,6 +1,5 @@
 <?php 
 	//Set API user and password
-	define("API_USER","21a8a02175");//21a8a02175
-	define("API_PASS","YbUN6Q74wolb");//YbUN6Q74wolb
-	//define("API_PASS","Em3mW5Xv9pS1");
+	define("API_USER","21a8a02175");
+	define("API_PASS","HQ6dZJyzVWDE");
 ?>
