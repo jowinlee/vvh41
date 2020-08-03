864b823932e245614cef99dea5fc6dac96a1d870
desu <denzdesu@gmail.com>
2020-08-04 00:22:27 +0800

    publishing update

--- pricing_page/thankyou.php
+++ pricing_page/thankyou.php
@@ -4,31 +4,28 @@
 <?php
 	session_start();
 	if (isset($_SESSION['site'])){
-		$_SESSION['site'] = $_SESSION['site'];
-	    $_SESSION['account'] = $_SESSION['account'];
+		$site = $_SESSION['site'];
+	    $account = $_SESSION['account'];
 	}else{
 		echo "no value";
+	}
+	if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	  	$siteName = $_POST['sitename'];
+	  	if (empty($siteName)) {
+	    	echo "SiteName is empty";
+	  	} else {
+	  		$sample = publishSite($siteName);
+	    	echo '<a href="'.$sample.'">'.$sample.'</a>';
+	  	}
 	}
 ?>
 <div class="container">
 	<h1>Thank You for your Subscription.</h1>
 	<p>You may now proceed to Publish your site.</p>
-	<p>Details: Account: <?php echo $_SESSION['account']; ?> sitename: <?php echo $_SESSION['site']; ?></p>
+	<p>Details: Account: <?php echo $account; ?> sitename: <?php echo $site; ?></p>
 	<form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
-		<input type="text" name="sitename" value="<?php echo $_SESSION['site']; ?>" hidden>
+		<input type="text" name="sitename" value="<?php echo $site; ?>" hidden>
 		<button type="submit" class="btn btn-primary">Publish my site now!</button>
 	</form>
-	<?php
-		if ($_SERVER["REQUEST_METHOD"] == "POST") {
-		  // collect value of input field
-		  $siteName = $_POST['sitename'];
-		  if (empty($siteName)) {
-		    echo "SiteName is empty";
-		  } else {
-		  	$sample = publishSite($siteName);
-		    echo '<a href="'.$sample.'">'.$sample.'</a>'
-		  }
-		}
-	?>
 </div>
 <?php include('../general/footer.php'); ?>
