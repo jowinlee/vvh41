ed1fb68e183f7631fc58491bba393d0b2af03a91
desu <denzdesu@gmail.com>
2020-08-03 23:26:19 +0800

    publishing update

--- pricing_page/index.php
+++ pricing_page/index.php
@@ -49,7 +49,7 @@
                             <h1 class="card-title">15.00€</h1>
                             <p class="card-text">/mensuel</p>
                         </div>
-                        <p class="text-center"><a data-ms-membership="5f26db89d9b4360004cd3f7d" href="signup.php" class="btn btn-elegant"> Subscribe </a><br>Free 15 Days Trial!</p>
+                        <p class="text-center"><a data-ms-membership="5f187cc6374769000482de6c" href="signup.php" class="btn btn-elegant"> Subscribe </a><br>Free 15 Days Trial!</p>
                         <?php include('../plans_page/personnel.php');?>
                         <div class="paypal-buttons px-2" data-desc="personel" data-subs="m" data-value="15.00"></div>
                     </div>
@@ -77,7 +77,7 @@
                             <h1 class="card-title">28.00€</h1>
                             <p class="card-text">/mensuel</p>
                         </div>
-                        <p class="text-center"><a href="#/ms/signup/5f12c0923b974a0004c9a504" class="btn btn-elegant"> Subscribe </a><br>Free 15 Days Trial!</p>
+                        <p class="text-center"><a data-ms-membership="5f12c0923b974a0004c9a504" href="signup.php" class="btn btn-elegant"> Subscribe </a><br>Free 15 Days Trial!</p>
                         <?php include('../plans_page/commerce_basic.php');?>
                         <div class="paypal-buttons px-2" data-desc="commercebasic" data-subs="m" data-value="28.00"></div>
                     </div>
@@ -91,7 +91,7 @@
                             <h1 class="card-title">40.00€</h1>
                             <p class="card-text">/mensuel</p>
                         </div>
-                        <p class="text-center"><a href="#/ms/signup/5f12c0f7169e140004728a68" class="btn btn-elegant"> Subscribe </a><br>Free 15 Days Trial!</p>
+                        <p class="text-center"><a data-ms-membership="5f12c0f7169e140004728a68" href="signup.php" class="btn btn-elegant"> Subscribe </a><br>Free 15 Days Trial!</p>
                         <?php include('../plans_page/commerce_avance.php');?>
                         <div class="paypal-buttons px-2" data-desc="commerceavance" data-subs="m" data-value="40.00"></div>
                     </div>
@@ -109,7 +109,7 @@
                             <h1 class="card-title">144.00€</h1>
                             <p class="card-text">12.00€ <small>/mensuel</small></p>
                         </div>
-                        <p class="text-center"><a href="#/ms/signup/5f12c015705fe10004a81a47" class="btn btn-elegant"> Subscribe </a><br> Free 15 Days Trial!</p>
+                        <p class="text-center"><a data-ms-membership="5f12c015705fe10004a81a47" href="signup.php" class="btn btn-elegant"> Subscribe </a><br> Free 15 Days Trial!</p>
                         <?php include('../plans_page/personnel.php');?>
                         <div class="paypal-buttons px-2" data-desc="personel" data-subs="y" data-value="144.00"></div>
                     </div>
@@ -123,7 +123,7 @@
                             <h1 class="card-title">204.00€</h1>
                             <p class="card-text">17.00€ <small>/mensuel</small></p>
                         </div>
-                        <p class="text-center"><a href="#/ms/signup/5f12c0518385a50004513f1f" class="btn btn-elegant"> Subscribe </a><br> Free 15 Days Trial!</p>
+                        <p class="text-center"><a data-ms-membership="5f12c0518385a50004513f1f" href="signup.php" class="btn btn-elegant"> Subscribe </a><br> Free 15 Days Trial!</p>
                         <?php include('../plans_page/entreprise.php');?>
                         <div class="paypal-buttons px-2" data-desc="entreprise" data-subs="y" data-value="204.00"></div>
                     </div>
@@ -137,7 +137,7 @@
                             <h1 class="card-title">288.00€</h1>
                             <p class="card-text">24.00€ <small>/mensuel</small></p>
                         </div>
-                        <p class="text-center"><a href="#/ms/signup/5f12c0bf169e140004728a67" class="btn btn-elegant"> Subscribe </a><br> Free 15 Days Trial!</p>
+                        <p class="text-center"><a data-ms-membership="5f12c0bf169e140004728a67" href="signup.php" class="btn btn-elegant"> Subscribe </a><br> Free 15 Days Trial!</p>
                         <?php include('../plans_page/commerce_basic.php');?>
                         <div class="paypal-buttons px-2" data-desc="commercebasic" data-subs="y" data-value="288.00"></div>
                     </div>
@@ -151,7 +151,7 @@
                             <h1 class="card-title">432.00€</h1>
                             <p class="card-text">36.00€ <small>/mensuel</small></p>
                         </div>
-                        <p class="text-center"><a href="#/ms/signup/5f12c129dfeab6000466d92e" class="btn btn-elegant"> Subscribe </a><br> Free 15 Days Trial!</p>
+                        <p class="text-center"><a data-ms-membership="5f12c129dfeab6000466d92e" href="signup.php" class="btn btn-elegant"> Subscribe </a><br> Free 15 Days Trial!</p>
                         <?php include('../plans_page/commerce_avance.php');?>
                         <div class="paypal-buttons px-2" data-desc="commerceavance" data-subs="y" data-value="432.00"></div>
                     </div>
